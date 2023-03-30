f0cd41d0cb0b813279b4230cf131bb19bb71d135
rabbani6020 <rabbani.cse.69@gmail.com>
2023-03-31 03:10:04 +0600

    run: prettier fix

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,19 @@
 const path = require('path');
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-postcss', '@storybook/addon-mdx-gfm'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-postcss',
+    '@storybook/addon-mdx-gfm',
+  ],
   framework: {
     name: '@storybook/nextjs',
     options: {
-      nextConfigPath: '/Users/golamrabbani/work-station/dev/front-end/nextjs-typescript-starter/next.config.js'
-    }
+      nextConfigPath:
+        '/Users/golamrabbani/work-station/dev/front-end/nextjs-typescript-starter/next.config.js',
+    },
   },
   staticDirs: ['../public'],
   /**
@@ -18,6 +25,6 @@ module.exports = {
    */
   env: () => ({}),
   docs: {
-    autodocs: false
-  }
+    autodocs: false,
+  },
 };
