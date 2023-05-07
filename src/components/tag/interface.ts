3525dc8e90b526be2cfdfb988767483dcb768d2f
rabbani6060 <rabbani@lemonhive.com>
2023-05-07 23:45:27 +0600

    update tag

--- src/components/tag/interface.ts
+++ src/components/tag/interface.ts
@@ -1,8 +1,4 @@
-export const tagColors = [
-  'bg-warning',
-  'bg-danger-light',
-  'bg-secondary',
-] as const;
+export const tagColors = ['bg-warning', 'bg-error', 'bg-secondary'] as const;
 type ITagColor = (typeof tagColors)[number];
 
 export interface ITag {
